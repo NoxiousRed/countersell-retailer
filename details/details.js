9e69d6d99c6ef01daf888ab1d15a85145c19b4c9
Craig Smith <craigshmitty6@gmail.com>
2024-02-15 12:12:16 -0500

    [MOD] Updated details page to pull info properly

--- details/details.js
+++ details/details.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     //set up what is going to be displayed where in the document
-    const cardDisplayContainer = document.getElementById('details-container')
+    const cardDisplayContainer = document.getElementById('content-container');
+    console.log(cardDisplayContainer);
 
     //grab the id value passed from the previous page:
     let id = new URLSearchParams(window.location.search).get('id');
     //parse out the details for the card associated with that id
-    let cardDetails = getCardDetails(id);
-    console.log(cardDetails);
+    getCardDetails(id)
+        .then(cardDetails => {createCardDetails(cardDetails, cardDisplayContainer)});
+
 
     async function getCardDetails(cardId) {
         const response = await fetch("../products/products.json")
@@ -19,6 +21,29 @@ document.addEventListener('DOMContentLoaded', function() {
                 return(products[i]);
             }
         }
+        //if no matching product is found, return default
+        return(products[0]);
+    }
+
+    function createCardDetails(cardDetails, displayContainer){
+         console.log(cardDetails);
+        console.log(displayContainer);
+        //create items for image and card details that populate based on id
+        const itemImage = document.createElement('img-container');
+        itemImage.id = "img-container"
+        itemImage.innerHTML = `<img src=${cardDetails.image}></img>`;
+        displayContainer.appendChild(itemImage);
+    
+        const itemDetails = document.createElement('card-details');
+        itemDetails.id = "card-details"
+        itemDetails.innerHTML = `<h1>${cardDetails.name}</h1>
+        <h3>Card Price: ${cardDetails.price}</h3>
+        <h3>${cardDetails.description}</h3>
+        <h3>Set Identifier: ${cardDetails.setIdentifier}</h3>
+        <h3>Set Year: ${cardDetails.setYear}</h3>`;
+        displayContainer.appendChild(itemDetails);
+
+        return itemDetails;
     }
 
 });
