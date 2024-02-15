757da3497ab5de2625eb2f36c54fe8737dfa7e3d
Craig Smith <craigshmitty6@gmail.com>
2024-02-15 11:26:23 -0500

    [MOD] Going to return to cart, returning to work on products page.

---
+++ cart/cart.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+
+});
+
+const updateButton = document.getElementById(update-quantity-btn);
+
+function updateQuantity() {
+
+}
+
+function deleteItem() {
+
+}
