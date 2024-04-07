5d80054761e7b1216bd71a4c87cc02204dc73968
Craig Smith <craigshmitty6@gmail.com>
2024-04-07 10:50:11 -0400

    set up first route for products

---
+++ public/products/products.js
@@ -0,0 +1,30 @@
+let products = null
+
+document.addEventListener('DOMContentLoaded', async function () {
+
+    const inventory = document.getElementById('list-products');
+
+    try {
+        const response = await fetch('http://localhost:3000/products');
+        products = await response.json;
+
+        console.log(products);
+        let listProduct = document.querySelector('.list-products')
+
+        products.forEach(card => {
+            const listItem = createListItem(card, listProduct)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+function createListItem(card, container) {
+    //link card to corresponding container
+    const listItem = document.createElement('p');
+    listItem.className = 'list-item';
+    listItem.innerHTML = `<img src = ${card.imageUrl} alt = "card-placeholder"><br>${card.productName}<br>${card.price}<br><a href="../details/details.html?id=${card.productId}">View Card</a>`;
+    container.appendChild(listItem);
+
+    return listItem;
+}
